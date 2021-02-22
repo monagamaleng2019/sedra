99b8bfb84b261d6143e3a9790bae8c6a414826bf
mona.gamal <mona.gamal1.mohamed.96@gmail.com>
2021-02-23 01:36:21 +0200

    add shared logo and button

---
+++ lib/shared_widgets/button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Button extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String label;
+  final Color backgroundColor;
+  Button(
+      {@required this.backgroundColor,
+      @required this.onPressed,
+      @required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      color: backgroundColor,
+      shape: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+}
---
+++ lib/shared_widgets/logo.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class Logo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("");
+  }
+}
